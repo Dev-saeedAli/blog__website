b77daaff94b2c609845aecd42a3d4e5c998668a1
saeed ali <mohammedsaidali69@gmail.com>
2022-11-26 19:33:12 +0400

    changed media queries for mobile menu and added the javascript logic for collapse

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const navList = document.querySelectorAll(".navlist");
+const subHeaders = document.querySelectorAll(".navlist .list-header .sub-list");
 const menuIcon = document.querySelector(".hamburger .menuIcon");
 const closeIcon = document.querySelector(".hamburger .closeIcon");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
+
+
 
 navList.forEach((listItem) => {
-  listItem.addEventListener("click", (e) => {
-    e.target.lastElementChild.classList.toggle("none");
-    e.target.lastElementChild.classList.toggle("active");
+  listItem.addEventListener("click", (event) => {
+    subHeaders.forEach((header) => {
+      header.classList.remove("active")
+      header.classList.add("none")
+    });
+    collapseNav(event)
   });
 });
 
+const collapseNav = (event) => {
+  const currentItem = event.target.lastElementChild;
+  if(currentItem){
+    currentItem.classList.toggle("none")
+    currentItem.classList.toggle("active");
+  }
+}
+
 menuIcon.addEventListener("click", ()=> {
-    nav.classList.add("active");
-    menuIcon.classList.add("none");
-    closeIcon.classList.remove("none");
+  nav.classList.add("active");
+  menuIcon.classList.add("none");
+  closeIcon.classList.remove("none");
 });
 
 closeIcon.addEventListener("click", ()=> {
